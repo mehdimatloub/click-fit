6edc948d72600c7db3973fb76763e98c2ae03947
mehdimatloub <104140239+mehdimatloub@users.noreply.github.com>
2025-02-02 19:00:07 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "160220", 
-    database: "clickfit_db",
-    port: 3307 
+    password: "your_password", 
+    database: "your_database",
+    port: 3306
 });
 
 db.connect((err) => {
